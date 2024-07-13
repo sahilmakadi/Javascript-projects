11afe9c389651899af1b4342efbbd17412bb5bc6
sahilmakadi <156317376+sahilmakadi@users.noreply.github.com>
2024-07-13 14:28:24 +0000

    new changes

--- 01_Basics/02_Datatypes.js
+++ 01_Basics/02_Datatypes.js
@@ -4,3 +4,22 @@
 
 console.log(3 + 3);
 console.log("Sahil");
+
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint more space
+// string => ""
+// boolean => true/false
+// null => standalone value  /  representation of empty value / not undefined but empty
+// undefined => 
+// symbol => uniqueness 
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof null); // object type
+console.log(typeof undefined); // undefined // because it is a datatype itself
