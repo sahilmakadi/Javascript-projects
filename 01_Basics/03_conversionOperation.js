d86eb84a1b9045b83e18432f243bbe85af03f067
sahilmakadi <156317376+sahilmakadi@users.noreply.github.com>
2024-07-13 19:11:24 +0000

    convertionof datatypes

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,5 +1,29 @@
-let score = "33"
-
+let score = "33abc" 
+// if null in score output 0
+// if undefined then nan 
 let valueInNumber = Number(score)
 
 console.log(typeof(valueInNumber));
+console.log(valueInNumber); // to check if input is number
+
+/*
+    "33"  -  33
+    "33abc" - NaN
+    true - 1; false = 0
+*/
+
+let isLoggedIn = 1
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn); 
+// 1- true
+// "" - false
+// "sahil" - false
+
+//if "sahil" is converted to Boolean - false
+
+let someNum = 33
+
+let stringNum = String(someNum)
+console.log(stringNum);
+console.log(typeof(stringNum));
