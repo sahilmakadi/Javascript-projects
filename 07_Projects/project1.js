1d0ef0af1374c9d622c0db6f659d060fffd8f864
sahilmakadi <156317376+sahilmakadi@users.noreply.github.com>
2024-07-26 19:13:46 +0000

    project 1

---
+++ 07_Projects/project1.js
@@ -0,0 +1,22 @@
+const allColors = document.querySelectorAll('.button')
+
+const body = document.querySelector('body')
+
+allColors.forEach((button)=>{
+    console.log(button);
+    button.addEventListener('click', function(e){
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id === 'grey') {
+            //body.style.backgroundColor= 'grey' // this is also same
+            body.style.backgroundColor= e.target.id
+        } else if (e.target.id === 'blue') {
+            document.body.style.backgroundColor = e.target.id
+        } else if (e.target.id === 'yellow') {
+            document.body.style.backgroundColor = 'yellow'
+        } else {
+            const white = document.querySelector('#white')
+            white.style.color = 'pink'
+        }
+    })
+})
